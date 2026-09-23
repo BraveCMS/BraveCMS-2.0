dc746b09b7e9ea681b578cd6b148e2e85b18677b
Razvan Zamfir <razvan_zamfir80@yahoo.com>
2026-09-23 10:56:51 +0300

    Add social login.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,39 +1,52 @@
 <?php
 
 namespace App\Providers;
+
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Pagination\Paginator; 
+use Illuminate\Pagination\Paginator;
+use App\Providers\FacebookProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+  /**
+   * Register any application services.
+   *
+   * @return void
+   */
+  public function register()
+  {
+    //
+  }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        // Database bug fix
-        Schema::defaultStringLength(191);
+  /**
+   * Bootstrap any application services.
+   *
+   * @return void
+   */
+  public function boot()
+  {
+    // Database bug fix
+    Schema::defaultStringLength(191);
 
-        // Use Twitter Bootstrap pagination
-				Paginator::useBootstrap();
+    // Use Twitter Bootstrap pagination
+    Paginator::useBootstrap();
 
-        Blade::if('userCan', function ($permission) {
-          return in_array($permission, Auth::user()->role->permissions->pluck('slug')->toArray());
-        });
-    }
+    $socialite = $this->app->make('Laravel\Socialite\Contracts\Factory');
+
+    $socialite->extend('facebook', function ($app) use ($socialite) {
+      $config = $app['config']['services.facebook'];
+
+      return $socialite->buildProvider(FacebookProvider::class, $config);
+    });
+
+    Blade::if('userCan', function ($permission) {
+      return in_array(
+        $permission,
+        Auth::user()->role->permissions->pluck('slug')->toArray()
+      );
+    });
+  }
 }
